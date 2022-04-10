3479580d179c38ba8b14b1fe41a70649e76f5961
christopher <cdnance1829@gmail.com>
2022-04-10 15:58:41 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function superbowlWin(array) {
+    const winnersCirle = [];
+    for (const item of array) {
+        if (item.result === "W") {
+            winnersCirle.push(item.year);
+        }
+    }
+
+    return winnersCirle[0];
+}
